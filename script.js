534c75d489e6e3590748d22442b0ce4c02f35714
ArjanElissen <arjanelissen@hotmail.com>
2022-04-07 11:20:04 +0200

    spirte

--- script.js
+++ script.js
@@ -23,76 +23,84 @@ let img = new Image();
 
 
 window.addEventListener('keydown', keyDownListener);
+
 function keyDownListener(event) {
     keyPresses[event.key] = true;
 }
 
 window.addEventListener('keyup', keyUpListener);
+
 function keyUpListener(event) {
     keyPresses[event.key] = false;
 }
 
 function loadImage() {
-  img.src = 'https://opengameart.org/sites/default/files/Green-Cap-Character-16x18.png';
-  img.onload = function() {
-    window.requestAnimationFrame(gameLoop);
-  };
+    img.src = 'https://opengameart.org/sites/default/files/Green-Cap-Character-16x18.png';
+    img.onload = function() {
+        window.requestAnimationFrame(gameLoop);
+    };
 }
 
 function drawFrame(frameX, frameY, canvasX, canvasY) {
-  ctx.drawImage(img,
-                frameX * WIDTH, frameY * HEIGHT, WIDTH, HEIGHT,
-                canvasX, canvasY, SCALED_WIDTH, SCALED_HEIGHT);
+    ctx.drawImage(img,
+        frameX * WIDTH, frameY * HEIGHT, WIDTH, HEIGHT,
+        canvasX, canvasY, SCALED_WIDTH, SCALED_HEIGHT);
 }
 
 loadImage();
 
 function gameLoop() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    let hasMoved = false;
 
-  let hasMoved = false;
-
-  if (keyPresses.w) {
-    moveCharacter(0, -MOVEMENT_SPEED, FACING_UP);
-    hasMoved = true;
-  } else if (keyPresses.s) {
-    moveCharacter(0, MOVEMENT_SPEED, FACING_DOWN);
-    hasMoved = true;
-  }
-
-  if (keyPresses.a) {
-    moveCharacter(-MOVEMENT_SPEED, 0, FACING_LEFT);
-    hasMoved = true;
-  } else if (keyPresses.d) {
-    moveCharacter(MOVEMENT_SPEED, 0, FACING_RIGHT);
-    hasMoved = true;
-  }
-
-  if (hasMoved) {
-    frameCount++;
-    if (frameCount >= FRAME_LIMIT) {
-      frameCount = 0;
-      currentLoopIndex++;
-      if (currentLoopIndex >= CYCLE_LOOP.length) {
-        currentLoopIndex = 0;
-      }
+    if (keyPresses.w) {
+        moveCharacter(0, -MOVEMENT_SPEED, FACING_UP);
+        hasMoved = true;
+    } else if (keyPresses.s) {
+        moveCharacter(0, MOVEMENT_SPEED, FACING_DOWN);
+        hasMoved = true;
     }
-  }
 
-  if (!hasMoved) {
-    currentLoopIndex = 0;
-  }
+    if (keyPresses.a) {
+        moveCharacter(-MOVEMENT_SPEED, 0, FACING_LEFT);
+        hasMoved = true;
+    } else if (keyPresses.d) {
+        moveCharacter(MOVEMENT_SPEED, 0, FACING_RIGHT);
+        hasMoved = true;
+    }
 
-  drawFrame(CYCLE_LOOP[currentLoopIndex], currentDirection, positionX, positionY);
-  window.requestAnimationFrame(gameLoop);
+    if (hasMoved) {
+        frameCount++;
+        if (frameCount >= FRAME_LIMIT) {
+            frameCount = 0;
+            currentLoopIndex++;
+            if (currentLoopIndex >= CYCLE_LOOP.length) {
+                currentLoopIndex = 0;
+            }
+        }
+    }
+
+    if (!hasMoved) {
+        currentLoopIndex = 0;
+    }
+
+    drawFrame(CYCLE_LOOP[currentLoopIndex], currentDirection, positionX, positionY);
+    window.requestAnimationFrame(gameLoop);
 }
 
 function moveCharacter(deltaX, deltaY, direction) {
-  if (positionX + deltaX > 0 && positionX + SCALED_WIDTH + deltaX < canvas.width) {
-    positionX += deltaX;
-  }
-  if (positionY + deltaY > 0 && positionY + SCALED_HEIGHT + deltaY < canvas.height) {
-    positionY += deltaY;
-  }
-  currentDirection = direction;
-} 
+    if (positionX + deltaX > 0 && positionX + SCALED_WIDTH + deltaX < canvas.width) {
+        positionX += deltaX;
+    }
+    if (positionY + deltaY > 0 && positionY + SCALED_HEIGHT + deltaY < canvas.height) {
+        positionY += deltaY;
+    }
+    currentDirection = direction;
+}
+
+function footer() {
+
+    console.log('hallo')
+    moveCharacter(MOVEMENT_SPEED, 0, FACING_RIGHT);
+    hasMoved = true;
+}
